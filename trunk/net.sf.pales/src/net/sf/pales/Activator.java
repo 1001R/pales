1910bc1313f5a11e0890fad14c4b92bd34589352
philipp.neuner@aimit.at <philipp.neuner@aimit.at@b04bb20d-258d-a19d-032f-4f99dcbd8a99>
2013-05-24 10:56:42 +0000

    Write Exec-Helper to temporary file at startup and pass its location to Pales so that the latter does not need to look for it in its data directory anymore

--- trunk/net.sf.pales/src/net/sf/pales/Activator.java
+++ trunk/net.sf.pales/src/net/sf/pales/Activator.java
@@ -1,11 +1,16 @@
 package net.sf.pales;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	private static Path execHelperPath;
 
 	static BundleContext getContext() {
 		return context;
@@ -14,10 +19,20 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		System.loadLibrary("pales");
+		try (InputStream is = bundleContext.getBundle().getEntry("/launcher/execw.exe").openStream()) {
+			Path tempFile = Files.createTempFile("pales", ".exe");
+			tempFile.toFile().deleteOnExit();
+			Files.copy(is, tempFile);
+			Activator.execHelperPath = tempFile;
+		}
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+	}
+	
+	public static Path getExecHelperPath() {
+		return execHelperPath;
 	}
 
 }
