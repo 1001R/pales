e4b2b833daab9f6bba394e2d1278529c09b949f0
philipp.neuner@aimit.at <philipp.neuner@aimit.at@b04bb20d-258d-a19d-032f-4f99dcbd8a99>
2014-05-08 07:04:45 +0000

    - Added Visual Studio solution - Implemented correct command line argument quoting under Windows quoted correctly now

--- net.sf.pales.jni/palesjni.h
+++ net.sf.pales.jni/palesjni.h
@@ -6,14 +6,8 @@
 extern "C" {
 #endif
 
-#ifdef WIN32
-#define PREFIX(x) _##x
-#else
-#define PREFIX(x) x
-#endif
-
-JNIEXPORT void JNICALL PREFIX(Java_net_sf_pales_OS_cancelProcess)(JNIEnv *, jclass, jstring, jlong);
-JNIEXPORT jlong JNICALL PREFIX(Java_net_sf_pales_ProcessManager_launch)(JNIEnv *, jclass, jstring, jstring, jstring, jstring, jstring, jstring, jstring, jobjectArray);
+JNIEXPORT void JNICALL Java_net_sf_pales_OS_cancelProcess(JNIEnv *, jclass, jstring, jlong);
+JNIEXPORT jlong JNICALL Java_net_sf_pales_ProcessManager_launch(JNIEnv *, jclass, jstring, jstring, jstring, jstring, jstring, jstring, jstring, jobjectArray);
 
 #ifdef __cplusplus
 }
